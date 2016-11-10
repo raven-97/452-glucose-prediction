cfb9f511da6fd6249c57ee0ecf27b83a868e13a6
Ryan Dick <rjdick95@yahoo.ca>
2016-11-10 14:51:31 -0500

    Added a function to implement all of the evaluation criteria.

--- FeedForwardNN.py
+++ FeedForwardNN.py
@@ -41,6 +41,10 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+NUM_EPOCHS = 1000 # Number of training epochs
+BATCH_SIZE = 100 # Use batch optimizer (weights are only update after every 10 inputs)
 
 # readData reads data from the specified pre-processed input data file.
 # The function returns an array of input data points and an array of the
@@ -66,6 +70,76 @@ def readData(filePath) :
     data = [x, y]
     return data;
 
+# evaluateNetwork runs the trained network on the the provided network and
+# reports the following evaluation metrics:
+#   - mean squared prediction error
+#   - percentage of lows that were correctly identified
+#   - percentage of highs that were corretly identified
+#   - number of falsely reported lows
+#   - number of falsely reported highs
+#
+# These metrics are defined as follows:
+#   - MSE:
+#       -> Average of (y_desired - y_actual)^2 for each test point
+#   - Low prediction accuracy:
+#       -> 100 * (Number of correct lows) / (Number of lows)
+#       -> Lows are any blood glucose level less than 70 mg/dL
+#   - High prediction accuracy:
+#       -> 100 * (Number of correct highs) / (Number of highs)
+#       -> Highs are any blood glucose level greater than 200
+#   - Number of false lows:
+#       -> Number of false lows where (y_desired - y_actual) > 6
+#       -> Note: false alarms are not counted if the prediction error is small
+#   - Number of false highs:
+#       -> Number of false highs where (y_actual - y_desired) > 6
+#       -> Note: false alarms are not counted if the prediciton error is small
+def evaluateNetwork(session, inData, outData, x, y, y_) :
+    # Compute mse:
+    mse = session.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: inData, y_: outData})
+    numTestPoints = len(inData)
+    numPredictedLows = 0
+    numLows = 0
+    numFalseLows = 0
+    numPredictedHighs = 0
+    numHighs = 0
+    numFalseHighs = 0
+    for i, inputPoint in enumerate(inData) :
+        # Apply network on current point:
+        predicted = session.run(y, feed_dict={x: [inputPoint]})
+        desired = outData[i][0]
+
+        # Update numLows, numHighs:
+        if(desired < 70) :
+            numLows += 1
+        elif(desired > 200) :
+            numHighs += 1
+
+        # Update prediction counts:
+        if(predicted < 70) : # If predicted low
+            if(desired < 70) : # If low prediction was correct
+                numPredictedLows += 1
+            elif((desired - predicted) > 6) : # If low prediction was incorrect and error was 'large'
+                numFalseLows += 1
+        elif(predicted > 200) : # If predicted high
+            if(desired > 200) : # If high prediction was correct
+                numPredictedHighs += 1
+            elif((predicted - desired) > 6) : # If high prediction was incorrect and error was 'large'
+                numFalseHighs += 1
+
+    # Print results:
+    print('Number of test points: ', numTestPoints)
+    print('Number of lows: ', numLows)
+    print('Number of highs: ', numHighs)
+    print("Number of 'normal' points: ", numTestPoints - numLows - numHighs)
+    print('') # New line
+    print('MSE: ', mse)
+    print('')
+    print('Low prediction accuracy: ', 100 * numPredictedLows / numLows, '%')
+    print('Number of false lows: ', numFalseLows)
+    print('')
+    print('High prediction accuracy: ', 100 * numPredictedHighs / numHighs, '%')
+    print('Number of false highs: ', numFalseHighs)
+# End evaluateNetwork(...)
 
 def main(_):
     # Import the training data and test data:
@@ -107,18 +181,30 @@ def main(_):
     ### Train ###
     sess = tf.InteractiveSession()
     tf.initialize_all_variables().run()
-    for i in range(3000): # 1000 training epochs
+    errors = []
+    for i in range(NUM_EPOCHS): # 1000 training epochs
+        # Train with one batch at a time (This was tested and found to be less effective than):
+        #for start, end in zip(range(0, len(trainData_in), BATCH_SIZE), range(BATCH_SIZE, len(trainData_in), BATCH_SIZE)):
+        #    sess.run(train_step, feed_dict={x: trainData_in[start:end], y_: trainData_out[start:end]})
         sess.run(train_step, feed_dict={x: trainData_in, y_: trainData_out})
-
+        mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
+        errors.append(mse)
+        #print(mse)
     ### Test ###
     # Apply network on all test data and compute MSE:
-    mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
-    print('MSE = ', mse)
-
+    #mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
+    #print('MSE = ', mse)
+    evaluateNetwork(sess, testData_in, testData_out, x, y, y_)
     # Output the desired and actual outputs for each test data point
-    for i, inputPoint in enumerate(testData_in) :
-        output = sess.run(y, feed_dict={x: [inputPoint]})
-        print('desired: ', testData_out[i], ', actual: ', output)
+    #for i, inputPoint in enumerate(testData_in) :
+    #    output = sess.run(y, feed_dict={x: [inputPoint]})
+    #    print('desired: ', testData_out[i], ', actual: ', output)
+
+    # Plot the MSE throughout training
+    #plt.plot(errors)
+    #plt.xlabel('#epochs')
+    #plt.ylabel('MSE')
+    #plt.show()
 
     # Uncomment this to compute the MSE for the current network on a different patient:
     #testData_in, testData_out = readData('tblADataRTCGM_Blind_Baseline_Split_output/78_test.csv')
