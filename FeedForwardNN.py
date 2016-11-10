40a8a12f2287b413740995caf3959eb14a0168ba
RyanJDick <rjdick95@yahoo.ca>
2016-11-10 02:06:15 -0500

    Added initial feed forward network attempt.
    This file successfully trains a feed forward network to predict glucose levels. The results are mediocre - as expected due to the noisiness of the data and number of unconsidered variables.

---
+++ FeedForwardNN.py
@@ -0,0 +1,130 @@
+#
+# CISC 452
+# Prediction of Blood Glusose Levels based on RTCGM Data
+#
+# November 10, 2016
+#
+# This script implements a multi-layer feed-forward neural network for glucose
+# prediciton
+#
+# The network has 7 input nodes and 1 output node. If the current time is 'T',
+# then the inputs and output represent the blood glucose measurements at the
+# following times:
+#   Inputs:     - T
+#               - (T - 10 mins)
+#               - (T - 20 mins)
+#               - (T - 30 mins)
+#               - (T - 40 mins)
+#               - (T - 60 mins)
+#               - (T - 90 mins)
+#
+#   Output:     - (T + 30 mins)
+#
+
+#
+# TODO:
+# 1. The training data should not all be loaded into memory at once in order for
+#   this to be scalable. (Do batches of ~100 at a time) There are examples of
+#   ways to do this online.
+# 2. The network actually does a really good job of predicting from what it's
+#   given. The large errors that are sometimes observed are usually caused by
+#   rapid spikes or drops. Perhaps we should try a shorter time horizon (like
+#   20 minutes) in order to try and reduce these errors.
+# 3. Add a performance measure that evaluates the percentage of lows that are
+#   succesfully identified (and counts the number of false positives).
+# 4. Understand how the Adam Optimizer works.
+# 5. Experiment with different node output functions. Currently, the nodes
+#   simply output their activations.
+#
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import tensorflow as tf
+
+# readData reads data from the specified pre-processed input data file.
+# The function returns an array of input data points and an array of the
+# corresponding desired outputs.
+def readData(filePath) :
+    x = []
+    y = []
+    with open(filePath, 'r') as f:
+        for line in f:
+            values = line.split(',')
+            time1 = float(values[0])
+            time2 = float(values[1])
+            time3 = float(values[2])
+            time4 = float(values[3])
+            time5 = float(values[4])
+            time6 = float(values[5])
+            time7 = float(values[6])
+            time8 = float(values[7])
+            newPointx = [time1, time2, time3, time4, time5, time6, time7] # Input
+            newPointy = [time8] # Desired Output
+            x.append(newPointx)
+            y.append(newPointy)
+    data = [x, y]
+    return data;
+
+
+def main(_):
+    # Import the training data and test data:
+    trainData_in, trainData_out = readData('tblADataRTCGM_Blind_Baseline_Split_output/1_train.csv')
+    testData_in, testData_out = readData('tblADataRTCGM_Blind_Baseline_Split_output/1_test.csv')
+
+    ### Create the model ###
+    x = tf.placeholder(tf.float32, [None, 7]) # Input placeholder
+
+    # Weights from inputs to first hidden layer (15 nodes):
+    Wh1 = tf.Variable(tf.random_uniform([7, 15], minval = -1, maxval = 1, dtype = tf.float32))
+    # Bias for first hidden layer:
+    bh1 = tf.Variable(tf.zeros([1, 15]))
+
+    # Weights from first hidden layer to second (15 nodes):
+    Wh2 = tf.Variable(tf.random_uniform([15, 15], minval = -1, maxval = 1, dtype = tf.float32)) # The weights from each of the 784 inputs to the 10 output nodes
+    # Bias for second hidden layer:
+    bh2 = tf.Variable(tf.zeros([1, 15])) # One bias input for each of the 10 output nodes
+
+    # Weights from second hidden layer to output layer (1 node):
+    Wo = tf.Variable(tf.random_uniform([15, 1], minval = -1, maxval = 1, dtype = tf.float32))
+    # Bias to output node:
+    bo = tf.Variable(tf.zeros([1, 1]))
+
+    # Nodes have no output function (they simply output their activation):
+    h1 = tf.add(tf.matmul(x, Wh1), bh1) # Hidden layer 1 output
+    h2 = tf.add(tf.matmul(h1, Wh2), bh2) # Hidden layer 2 output
+    y = tf.add(tf.matmul(h2, Wo), bo) # Network output
+
+    y_ = tf.placeholder(tf.float32, [None, 1]) # Desired output placeholder
+
+    # Error function to be minimized is the mean square error:
+    loss = tf.reduce_mean(tf.square(y - y_))
+
+    # Define training algorithm (Adam Optimizer):
+    #train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+    train_step = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
+
+    ### Train ###
+    sess = tf.InteractiveSession()
+    tf.initialize_all_variables().run()
+    for i in range(3000): # 1000 training epochs
+        sess.run(train_step, feed_dict={x: trainData_in, y_: trainData_out})
+
+    ### Test ###
+    # Apply network on all test data and compute MSE:
+    mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
+    print('MSE = ', mse)
+
+    # Output the desired and actual outputs for each test data point
+    for i, inputPoint in enumerate(testData_in) :
+        output = sess.run(y, feed_dict={x: [inputPoint]})
+        print('desired: ', testData_out[i], ', actual: ', output)
+
+    # Uncomment this to compute the MSE for the current network on a different patient:
+    #testData_in, testData_out = readData('tblADataRTCGM_Blind_Baseline_Split_output/78_test.csv')
+    #mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
+    #print('MSE2 = ', mse)
+
+
+if __name__ == '__main__':
+    tf.app.run()
