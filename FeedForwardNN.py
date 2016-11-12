abc8f96d73bce3a83e1cdf1f22a73694aa72a18f
Ryan Dick <rjdick95@yahoo.ca>
2016-11-12 11:24:15 -0500

    Cleaned up the code to improve readability.

--- FeedForwardNN.py
+++ FeedForwardNN.py
@@ -15,26 +15,10 @@
 #               - (T - 20 mins)
 #               - (T - 30 mins)
 #               - (T - 40 mins)
+#               - (T - 50 mins)
 #               - (T - 60 mins)
-#               - (T - 90 mins)
 #
-#   Output:     - (T + 30 mins)
-#
-
-#
-# TODO:
-# 1. The training data should not all be loaded into memory at once in order for
-#   this to be scalable. (Do batches of ~100 at a time) There are examples of
-#   ways to do this online.
-# 2. The network actually does a really good job of predicting from what it's
-#   given. The large errors that are sometimes observed are usually caused by
-#   rapid spikes or drops. Perhaps we should try a shorter time horizon (like
-#   20 minutes) in order to try and reduce these errors.
-# 3. Add a performance measure that evaluates the percentage of lows that are
-#   succesfully identified (and counts the number of false positives).
-# 4. Understand how the Adam Optimizer works.
-# 5. Experiment with different node output functions. Currently, the nodes
-#   simply output their activations.
+#   Output:     - (T + 20 mins)
 #
 
 from __future__ import absolute_import
@@ -44,14 +28,13 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 NUM_EPOCHS = 1000 # Number of training epochs
-BATCH_SIZE = 100 # Use batch optimizer (weights are only update after every 10 inputs)
 
 # readData reads data from the specified pre-processed input data file.
 # The function returns an array of input data points and an array of the
 # corresponding desired outputs.
 def readData(filePath) :
-    x = []
-    y = []
+    x_data = []
+    y_data = []
     with open(filePath, 'r') as f:
         for line in f:
             values = line.split(',')
@@ -65,9 +48,9 @@ def readData(filePath) :
             time8 = float(values[7])
             newPointx = [time1, time2, time3, time4, time5, time6, time7] # Input
             newPointy = [time8] # Desired Output
-            x.append(newPointx)
-            y.append(newPointy)
-    data = [x, y]
+            x_data.append(newPointx)
+            y_data.append(newPointy)
+    data = [x_data, y_data]
     return data;
 
 # evaluateNetwork runs the trained network on the the provided network and
@@ -93,9 +76,9 @@ def readData(filePath) :
 #   - Number of false highs:
 #       -> Number of false highs where (y_actual - y_desired) > 6
 #       -> Note: false alarms are not counted if the prediciton error is small
-def evaluateNetwork(session, inData, outData, x, y, y_) :
+def evaluateNetwork(session, inData, outData, prediction) :
     # Compute mse:
-    mse = session.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: inData, y_: outData})
+    mse = session.run(tf.reduce_mean(tf.square(prediction - y_desired)), feed_dict={x: inData, y_desired: outData})
     numTestPoints = len(inData)
     numPredictedLows = 0
     numLows = 0
@@ -105,7 +88,7 @@ def evaluateNetwork(session, inData, outData, x, y, y_) :
     numFalseHighs = 0
     for i, inputPoint in enumerate(inData) :
         # Apply network on current point:
-        predicted = session.run(y, feed_dict={x: [inputPoint]})
+        predicted = session.run(prediction, feed_dict={x: [inputPoint]})
         desired = outData[i][0]
 
         # Update numLows, numHighs:
@@ -141,14 +124,13 @@ def evaluateNetwork(session, inData, outData, x, y, y_) :
     print('Number of false highs: ', numFalseHighs)
 # End evaluateNetwork(...)
 
-def main(_):
-    # Import the training data and test data:
-    trainData_in, trainData_out = readData('tblADataRTCGM_Unblinded_ControlGroup_1_output_20/151_train.csv')
-    testData_in, testData_out = readData('tblADataRTCGM_Unblinded_ControlGroup_1_output_20/151_test.csv')
+x = tf.placeholder(tf.float32, [None, 7], name='x') # Input placeholder
+y_desired = tf.placeholder(tf.float32, [None, 1], name='y_desired') # Desired output placeholder
 
-    ### Create the model ###
-    x = tf.placeholder(tf.float32, [None, 7]) # Input placeholder
-
+# feedForwardNN describes the model of the feed forward neural network being
+# used. The selected architecture consists of two hidden layers containing 15
+# nodes each. All nodes employ a linear activation function.
+def feedForwardNN(x) :
     # Weights from inputs to first hidden layer (15 nodes):
     Wh1 = tf.Variable(tf.random_uniform([7, 15], minval = -1, maxval = 1, dtype = tf.float32))
     # Bias for first hidden layer:
@@ -167,50 +149,60 @@ def main(_):
     # Nodes have no output function (they simply output their activation):
     h1 = tf.add(tf.matmul(x, Wh1), bh1) # Hidden layer 1 output
     h2 = tf.add(tf.matmul(h1, Wh2), bh2) # Hidden layer 2 output
-    y = tf.add(tf.matmul(h2, Wo), bo) # Network output
+    output = tf.add(tf.matmul(h2, Wo), bo) # Network output
 
-    y_ = tf.placeholder(tf.float32, [None, 1]) # Desired output placeholder
+    return output
+
+def trainFFNN(x):
+    # Import the training data and test data:
+    # 151, 149, 174
+    trainData_in, trainData_out = readData('tblADataRTCGM_Unblinded_ControlGroup_1_output_RNN_20/174_train.csv')
+    testData_in, testData_out = readData('tblADataRTCGM_Unblinded_ControlGroup_1_output_RNN_20/174_test.csv')
+
+    prediction = feedForwardNN(x)
 
     # Error function to be minimized is the mean square error:
-    loss = tf.reduce_mean(tf.square(y - y_))
+    loss = tf.reduce_mean(tf.square(prediction - y_desired))
 
     # Define training algorithm (Adam Optimizer):
+    # Note: AdamOptimizer produced better results than the GradientDescentOptimizer
     #train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
     train_step = tf.train.AdamOptimizer(learning_rate=0.1).minimize(loss)
 
-    ### Train ###
+    # Train:
+    errors = []
     sess = tf.InteractiveSession()
     tf.initialize_all_variables().run()
-    errors = []
     for i in range(NUM_EPOCHS): # 1000 training epochs
-        # Train with one batch at a time (This was tested and found to be less effective than):
+        ### Batch training was tested, but per-epoch produced better results:
+        # Train with one batch at a time:
         #for start, end in zip(range(0, len(trainData_in), BATCH_SIZE), range(BATCH_SIZE, len(trainData_in), BATCH_SIZE)):
-        #    sess.run(train_step, feed_dict={x: trainData_in[start:end], y_: trainData_out[start:end]})
-        sess.run(train_step, feed_dict={x: trainData_in, y_: trainData_out})
-        mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
-        errors.append(mse)
-        print(mse)
-    ### Test ###
-    # Apply network on all test data and compute MSE:
-    #mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
-    #print('MSE = ', mse)
-    evaluateNetwork(sess, testData_in, testData_out, x, y, y_)
+        #    sess.run(train_step, feed_dict={x: trainData_in[start:end], y_desired: trainData_out[start:end]})
+
+        # Per-Epoch training:
+        sess.run(train_step, feed_dict={x: trainData_in, y_desired: trainData_out})
+        # Print MSE on test data after every 10 epochs
+        if i % 10 == 0 :
+            mse = sess.run(tf.reduce_mean(tf.square(prediction - y_desired)), feed_dict={x: testData_in, y_desired: testData_out})
+            errors.append(mse)
+            print(mse)
+
     # Output the desired and actual outputs for each test data point
     #for i, inputPoint in enumerate(testData_in) :
     #    output = sess.run(y, feed_dict={x: [inputPoint]})
     #    print('desired: ', testData_out[i], ', actual: ', output)
+
+    # Test:
+    evaluateNetwork(sess, testData_in, testData_out, prediction)
+    # Uncomment this to evaluate the current network on a different patient:
+    #testData_in, testData_out = readData('tblADataRTCGM_Blind_Baseline_Split_output/78_test.csv')
+    #evaluateNetwork(sess, testData_in, testData_out, prediciton)
 
     # Plot the MSE throughout training
     #plt.plot(errors)
     #plt.xlabel('#epochs')
     #plt.ylabel('MSE')
     #plt.show()
+# End trainFFNN(x)
 
-    # Uncomment this to compute the MSE for the current network on a different patient:
-    #testData_in, testData_out = readData('tblADataRTCGM_Blind_Baseline_Split_output/78_test.csv')
-    #mse = sess.run(tf.reduce_mean(tf.square(y - y_)), feed_dict={x: testData_in, y_: testData_out})
-    #print('MSE2 = ', mse)
-
-
-if __name__ == '__main__':
-    tf.app.run()
+trainFFNN(x)
